537cbf18597292085d1c0341dd8d92e88e1b95a6
Craig Hobbs <craigahobbs@gmail.com>
2021-12-02 11:43:46 -0800

    schema-markdown-doc 1.0.7

---
+++ element-app/README.md
@@ -0,0 +1,18 @@
+# element-app
+
+[![npm](https://img.shields.io/npm/v/element-app)](https://www.npmjs.com/package/element-app)
+[![GitHub](https://img.shields.io/github/license/craigahobbs/element-app)](https://github.com/craigahobbs/element-app/blob/main/LICENSE)
+
+[element-app API Documentation](https://craigahobbs.github.io/element-app/)
+
+Coming soon!
+
+
+## Development
+
+This project is developed using [javascript-build](https://github.com/craigahobbs/javascript-build#readme). It was started
+using [javascript-template](https://github.com/craigahobbs/javascript-template#readme) as follows:
+
+```
+template-specialize javascript-template/template/ element-app/ -k package element-app -k name 'Craig A. Hobbs' -k email 'craigahobbs@gmail.com' -k github 'craigahobbs' -k noapp 1
+```
---
+++ element-app/lib/app.js
@@ -0,0 +1,169 @@
+// Licensed under the MIT License
+// https://github.com/craigahobbs/element-app/blob/main/LICENSE
+
+/** @module lib/app */
+
+import {SchemaMarkdownParser, decodeQueryString, encodeQueryString, validateType} from '../../schema-markdown/index.js';
+import {renderElements} from '../../element-model/index.js';
+
+
+/**
+ * The Element Model Application base class
+ *
+ * @property {Object} window - The web browser window object
+ * @property {string} title - The default application title
+ * @property {string} hashType - The application hash parameters' schema type name (in "hashTypes")
+ * @property {Object} hashTypes - The application hash parameters' Schema Markdown type model
+ * @property {Object} params - The validated hash parameters object
+ */
+export class ElementApplication {
+    /**
+     * Create an application instance
+     *
+     * @param {Object} window - The web browser window object
+     * @param {string} title - The default application title
+     * @param {string} hashType - The application hash parameters' schema type name (in "hashTypes")
+     * @param {Object} hashTypes - The application hash parameters' Schema Markdown type model
+     */
+    constructor(window, title, hashType, hashTypes) {
+        this.window = window;
+        this.title = title;
+        this.hashType = hashType;
+        if (typeof hashTypes === 'string') {
+            this.hashTypes = (new SchemaMarkdownParser(hashTypes)).types;
+        } else {
+            this.hashTypes = hashTypes;
+        }
+        this.params = null;
+    }
+
+    /**
+     * Run the application. This method calls render and subscribes to any hash parameter changes to
+     * re-render on any hash parameter change.
+     */
+    async run() {
+        await this.render();
+        this.window.addEventListener('hashchange', () => this.render(), false);
+    }
+
+    /**
+     * Render the application. This method calls updateParams, main, and preRender prior to calling
+     * element-model renderElements.
+     */
+    async render() {
+        let result;
+        let isError = false;
+        try {
+            // Validate hash parameters
+            const paramsPrev = this.params;
+            this.updateParams();
+
+            // Skip the render if the page params haven't changed
+            if (paramsPrev !== null && JSON.stringify(paramsPrev) === JSON.stringify(this.params)) {
+                return;
+            }
+
+            // Render the application elements
+            result = await this.main();
+        } catch ({message}) {
+            result = {'elements': {'html': 'p', 'elem': {'text': `Error: ${message}`}}};
+            isError = true;
+        }
+
+        // Navigate?
+        if ('location' in result && result.location !== null) {
+            this.window.location.href = result.location;
+            return;
+        }
+
+        // Set the window title
+        this.window.document.title = 'title' in result && result.title !== null ? result.title : this.title;
+
+        // On-render notification
+        this.preRender();
+
+        // Render the element model
+        renderElements(
+            this.window.document.body,
+            'elements' in result ? result.elements : {'html': 'p', 'elem': {'text': 'No elements'}}
+        );
+
+        // If there is a URL hash ID, re-navigate to go there since it was just rendered. After the
+        // first render, re-render is short-circuited by the unchanged hash param check above.
+        if (!isError && getHashID(this.window.location.hash) !== null) {
+            this.window.location.href = this.window.location.hash;
+        }
+    }
+
+    /**
+     * Parse and validate the hash parameters
+     *
+     * @param {?string} paramString - Optional parameter string for testing
+     */
+    updateParams(paramString = null) {
+        // Clear, then validate the hash parameters (may throw)
+        this.params = null;
+
+        // Decode the params string
+        const params = decodeQueryString(paramString !== null ? paramString : this.window.location.hash.slice(1));
+
+        // Validate the params
+        this.params = validateType(this.hashTypes, this.hashType, params);
+    }
+
+    /**
+     * @typedef MainResult
+     * @property {string} elements - The application's element model
+     * @property {string} [location] - The URL to navigate to
+     * @property {string} [title] - The window title
+     */
+
+    /**
+     * Override-able application main entry point. The default implementation renders nothing.
+     *
+     * @abstract
+     * @returns {module:lib/app~MainResult}
+     */
+    // eslint-disable-next-line class-methods-use-this, require-await
+    async main() {
+        return {'elements': null};
+    }
+
+    /**
+     * Override-able method that is called prior to application element model render. The default
+     * implementation does nothing.
+     *
+     * @abstract
+     */
+    // eslint-disable-next-line class-methods-use-this
+    preRender() {
+        // Default implementation does nothing
+    }
+}
+
+
+/**
+ * Get a URL's hash ID
+ *
+ * @param {string} url - The URL
+ * @returns {?string} The URL's hash ID, or null if not found
+ */
+export function getHashID(url) {
+    const matchId = url.match(rHashId);
+    return matchId !== null ? matchId[1] : null;
+}
+
+const rHashId = /[#&]([^=]+)$/;
+
+
+/**
+ * Encode hash parameters with a hash ID
+ *
+ * @param {Object} params - The hash parameters
+ * @param {?string} hashID - The hash ID
+ * @returns {string} The encoded hash ID
+ */
+export function encodeHashID(params, hashID) {
+    const paramString = encodeQueryString(params);
+    return `#${paramString}${hashID !== null && paramString !== '' ? '&' : ''}${hashID !== null ? hashID : ''}`;
+}
